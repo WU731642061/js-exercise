6f7711076f02e3ad4baad6b2117bdc0ab0e85a67
wuyiwen <731642061@163.com>
2019-11-04 00:25:00 +0800

    实现apply和call

--- knowledge/call_apply.js
+++ knowledge/call_apply.js
@@ -58,15 +58,48 @@ slice(arguments);
 // 接下来自己实现一个call，apply和bind
 // 首先，call，apply，bind是在Function原型上的方法，所以希望自己实现call，apply和bind也必须修改Function的原型
 
-Function.prototype.newCall = function(){
+Function.prototype.newCall = function(context){
+  // 第一步，获取这个对象，如果为null，则将对象挂载再window上
+  var context = context || window
+  // 这里的this指向的是调用的函数本身，相当于xxx = new Function，在new的时候，this指向了xxx
+  // 这里需要谨慎考虑的是，context也可能有fn这个key，这里可能会覆盖对象本身的key，在实际操作中，建议使用生成随机数来实现这边this指向的改变，这里就不做这个实现
+  // var uniqueID = "00" + Math.random();
+  // while (context.hasOwnProperty(uniqueID)) {
+  //   uniqueID = "00" + Math.random();
+  // }
+  // context[uniqueID] = this;
+  context.fn = this
+  var args = [...arguments].slice(1)
+  // 或者 new Array(...arguments).slice(1)
+  // 或者 Array.from(arguments).slice(1)        from方法从一个类似数组或可迭代对象创建一个新的，浅拷贝的数组实例
+  var r = context.fn(...args)
+  // 如果不允许使用es6特性，可以使用eval来实现
+  // var args = [];
+  // for (var i = 1; i < arguments.length; i++) {  
+  //   args.push("arguments[" + i + "]");
+  // }
+  // var result = eval("context.fn(" + args + ")");
+  // 最后删除context的fn属性
+  delete context.fn
+  return r
+}
+
+Function.prototype.newApply = function(context, arr){
+  // apply和call十分的相似，只是多了一步检查arr是否存在
+  var context = context || window
+  context.fn = this
+
+  // 这里需要判断传入的数据是否为数组，如果不是要报错，但是typeof无法检测出数组的类型，需要用instanceof
+  if (arr && !arr instanceof Array){
+    throw "请传一个正确的数组"
+  }
+  var r = arr ? context.fn(...arr) : context.fn()
+  return r
+}
+
+Function.prototype.newBind = function(context){
 
 }
 
-Function.prototype.newApply = function(){
-
-}
-
-Function.prototype.newBind = function(){
-
-}
+// 文章参考自：https://zhuanlan.zhihu.com/p/83523272
 
