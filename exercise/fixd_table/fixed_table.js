25c9592a861e7472bb8d3365fad4f425f98b3be3
wuyiwen <731642061@qq.com>
2021-02-22 16:45:22 +0800

    固定表头和固定列

---
+++ exercise/fixd_table/fixed_table.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: yiwen.wu
+ * @Date: 2021-02-22 10:22:42
+ * @LastEditTime: 2021-02-22 10:22:42
+ * @LastEditors: yiwen.wu
+ * @Description: 
+ * @FilePath: /js-exercise/exercise/fixd_columu_table/fixd_columu_table.js
+ */
