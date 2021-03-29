f691b3863f906e3671f1995716325b2a30e7f2fd
wuyiwen <731642061@qq.com>
2021-03-29 16:02:18 +0800

    add: canvas related file; add: js program design doc

---
+++ exercise/canvas/basic/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: yiwen.wu
+ * @Date: 2020-12-24 14:49:55
+ * @LastEditTime: 2020-12-24 14:49:55
+ * @LastEditors: yiwen.wu
+ * @Description: 
+ * @FilePath: /js-exercise/exercise/canvas/basic/index.js
+ */
